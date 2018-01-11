06a30aca398826765b1f05cfa593f28646e32eb6
Jason Flax <jsflax@gmail.com>
2018-01-11 14:48:14 -0500

    Updated for Swift 4

--- Package.swift
+++ Package.swift
@@ -1,17 +1,20 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-	name: "Swamp",
-	targets: [
-		Target(
-			name: "Swamp",
-			dependencies: []
-		)
+    name: "Swamp",
+    dependencies: [
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", Version(1, 0, 0)..<Version(3, .max, .max)),
+        .package(url: "https://github.com/daltoniam/Starscream", Version(1, 0, 0)..<Version(3, .max, .max)),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", .upToNextMinor(from: "0.8.0"))
 	],
-	dependencies: [
-        .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", versions: Version(1,0,0)..<Version(3, .max, .max)),
-		.Package(url: "https://github.com/daltoniam/Starscream", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/krzyzanowskim/CryptoSwift", majorVersion: 0, minor: 6),
-	],
-	exclude: ["Example"]
+    targets: [
+        .target(
+            name: "Swamp",
+            dependencies: ["SwiftyJSON", "Starscream", "CryptoSwift"],
+            path: "."
+        )
+    ]
 )
